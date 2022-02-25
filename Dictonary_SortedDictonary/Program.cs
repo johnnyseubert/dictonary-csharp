1ccc88f6841e6998d738c32d463d3adc0b3a561f
DATA53454 <data53454@datainfo.inf.br>
2022-02-25 16:48:29 -0300

    adicionando arquivos

---
+++ Dictonary_SortedDictonary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictonary_SortedDictonary
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            try
+            {
+                cookies["user"] = "Maria";
+                cookies["email"] = "maria@gmail.com";
+                cookies["phone"] = "99219-6789";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Size: " + cookies.Count);
+            
+            Console.WriteLine("All Cookies: ");
+            foreach (KeyValuePair<string,string> item in cookies)
+            {
+                Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            }
+        }
+    }
+}
